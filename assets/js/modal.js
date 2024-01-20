164e4591a50e9e58ff738097ef9a076ff712b9a6
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2024-01-20 21:08:17 +0600

    modal added, hover animation added, on projects

---
+++ assets/js/modal.js
@@ -0,0 +1,31 @@
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "block";
+        // Disable scrolling when the modal is open
+        document.body.style.overflow = "hidden";
+    }
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+        // Enable scrolling when the modal is closed
+        document.body.style.overflow = "auto";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all modals with class "modal"
+    var modals = document.querySelectorAll(".modal");
+
+    // When the user clicks anywhere outside of a modal, close it
+    window.addEventListener("click", function (event) {
+        modals.forEach(function (modal) {
+            if (event.target === modal) {
+                closeModal(modal.id);
+            }
+        });
+    });
+});
